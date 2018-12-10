d34780e16e3197367a0c6f3f7250278a8a8cf163
Yoni <yoniifriedman@gmail.com>
2018-12-09 23:58:38 -0500

    debugging weight updates

--- code/particle_filter.py
+++ code/particle_filter.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -17,70 +18,33 @@ from Map import Map_2D
 class Particle_Filter():
     x_start = 0
     y_start = 0
-    scan_data = []
+    messages = []   
     particles = []
-
+    scan_noise = 0.1
+    
     #Initialize filter with n uniformly distributed particles
     def __init__(self, _map, n):
+        self.n = n
         for i in range(n):
             particle = Particle(random.uniform(_map.min_x, _map.max_x), \
                                 random.uniform(_map.min_y, _map.max_y), \
                                 random.uniform(0, 2*pi))
             self.particles.append(particle)
 
-    #def compute_weights(self, )
-
-
-class Particle:
-    x = 0
-    y = 0
-    theta = 0
-    weight = 0
-    distances = []
-
-    def __init__(self,x,y,theta):
-        self.x = x
-        self.y = y
-        self.theta = theta
-
-    def scan(self, _map):
-        obstacles = _map.obstacles
-        #Take scans from -30 to 30 degrees at 1.125 intervals
-        scan_angle = 30
-        for i in range(54):
-            angle = self.theta - scan_angle
-            scan_point = (10*math.cos(angle),  10*math.sin(angle))
-            line = LineString([(self.x, self.y), scan_point])
-            linestring_type = type(line)
-            for o in obstacles:
-                collision = line.intersection(o)
-                if collision:
-                    #Compute distance
-                    if type(collision) != linestring_type:
-                        l = []
-                        for i in collision.geoms:
-                            linestring = (i.coords[0], i.coords[1])
-                            l.append(linestring)
-                    else:
-                        l = [(collision.coords[0], collision.coords[1])]
-                    min_dist = self.min_distance(l)
-                    self.distances.append(min_dist)
-
-            scan_angle -= 1.125
-        
-    def min_distance(self, points):
-        #print 'POINTS:' , points
-        min_dist = 100000
-        for i in range(len(points)):
-            for j in range(len(points[i])):
-                #print points[i][j]
-                dist = math.sqrt((self.x - points[i][j][0])**2 + (self.y - points[i][j][1])**2) 
-                if dist < min_dist:
-                    #print 'Swapping distances. \tCurrent min: ', min_dist, '\tNew min: ', dist
-                    min_dist = dist
-        #print 
-        #print
-        return dist
+    def compute_weights(self, message):
+        mean_robot_dist = sum([distance for distance in message.scan_data if not math.isnan(distance)]) / len(message.scan_data)
+        std = self.scan_noise
+        for particle in self.particles:
+            mean_particle_dist = sum([distance for distance in particle.distances if not math.isnan(distance)]) 
+            mean_particle_dist /= len(particle.distances)
+            d = mean_particle_dist - mean_robot_dist
+            weight = 1/(math.sqrt(2*pi)*std)*math.e**-(d**2/(2*std**2))
+            particle.weight = weight / self.n
+            
+        print "Particles sum to: "
+        p = [particle.weight for particle in self.particles]
+        print p
+        print sum(p)
 
 class Message():
     heading = 0
@@ -94,6 +58,58 @@ class Message():
     noisy_distance = 0
 
     scan_data = []
+
+class Particle:
+    x = 0
+    y = 0
+    theta = 0
+    weight = 0
+    distances = []  #Maintained as a min heap
+
+    def __init__(self,x,y,theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
+
+    def scan(self, _map):
+        self.distances = []     #Refresh distance list each time scan is called
+        obstacles = _map.obstacles
+        #Take scans from -30 to 30 degrees at 1.125 intervals
+        scan_angle = 30
+        for i in range(54):
+            angle = self.theta - scan_angle
+            scan_point = (10*math.cos(angle),  10*math.sin(angle))
+            line = LineString([(self.x, self.y), scan_point])
+            linestring_type = type(line)    #used to check if intersection is shapely LineString object or MultiLineString object
+            for o in obstacles:
+                collision = line.intersection(o)
+                if collision:
+                    #Compute distance
+                    if type(collision) != linestring_type:  #MultiLineStringObject (we may pass through two obstacles)
+                        l = []
+                        for i in collision.geoms:
+                            linestring = (i.coords[0], i.coords[1])
+                            l.append(linestring)
+                    else:
+                        l = [(collision.coords[0], collision.coords[1])]
+                    distance = self.nearest_collision_point_on_line(l)
+                    hq.heappush(self.distances, distance)
+                else:
+                    distance = float('NaN')
+                    hq.heappush(self.distances, distance)
+            scan_angle -= 1.125
+        
+    def nearest_collision_point_on_line(self, points):
+        #print 'POINTS:' , points
+        min_dist = 100000
+        for i in range(len(points)):
+            for j in range(len(points[i])):
+                #print points[i][j]
+                dist = math.sqrt((self.x - points[i][j][0])**2 + (self.y - points[i][j][1])**2) 
+                if dist < min_dist:
+                    #print 'Swapping distances. \tCurrent min: ', min_dist, '\tNew min: ', dist
+                    min_dist = dist
+        return dist
 
 
 #Param: Path to trajectory file
@@ -130,9 +146,10 @@ def parse_trajectories(path_to_trajectory_file, pf):
         message.angular_twist = [float(line.split(':')[1]) for line in raw_message[20:23]]
         message.noisy_heading = float(raw_message[25].split(':')[1])
         message.noisy_distance = float(raw_message[27].split(':')[1])
-        message.scan_data = [float(elem) for elem in raw_message[29].split(':')[1][2:-2].split(',')]
-
-        pf.scan_data.append(message)
+        unordered_scan_data = [float(elem) for elem in raw_message[29].split(':')[1][2:-2].split(',')]
+        for sd in unordered_scan_data:
+            hq.heappush(message.scan_data, sd)
+        pf.messages.append(message)
 
 def main():
     number = raw_input("Which map do you want to visualize? \nEnter 1, 2, 3, 4, 5, 6 or 7 to continue: ")
@@ -142,11 +159,14 @@ def main():
     #map1.plot()
 
     f2 = '../turtlebot_maps/trajectories/trajectories_'+str(number)+'.txt'
-    pf = Particle_Filter(map1, 1000)
+    start = time.time()
+    pf = Particle_Filter(map1, 100)
     parse_trajectories(f2, pf)
-    
-    for particle in pf.particles[:10]:
+    for particle in pf.particles:
         particle.scan(map1)
+    pf.compute_weights(pf.messages[0])
+    end = time.time()
+    print ("Total time taken: ", end - start)
     #print pf.particles
 
 if __name__=='__main__':
