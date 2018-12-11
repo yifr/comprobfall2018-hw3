aa9e3e96b5a3faa4020d0905045d7dbed9d9ccdb
Yoni <yoniifriedman@gmail.com>
2018-12-10 23:52:55 -0500

    Fixed scanning, added walls as obstacles, and fixed degree/radians bug

--- code/particle_filter.py
+++ code/particle_filter.py
@@ -38,7 +38,6 @@ class Particle_Filter():
             particle.scan(_map)
 
     def compute_weights(self, message):
-        print len(message.scan_data)
         mean_robot_dist = sum([distance for distance in message.scan_data if not math.isnan(distance)]) / len(message.scan_data)
         std = self.scan_noise
         for particle in self.particles:
@@ -49,14 +48,12 @@ class Particle_Filter():
             weight = 1/(math.sqrt(2*pi)*std)*math.e**-(d**2/(2*std**2))
             particle.weight = weight / self.n
         
-         #Normalize weights:
+        #Normalize weights:
         weights = [particle.weight for particle in self.particles]
         total_weight = sum(weights)
         for particle in self.particles:
             particle.weight /= total_weight
             
-
-
 class Particle:
     x = 0
     y = 0
@@ -76,20 +73,85 @@ class Particle:
         scan_angle = 30
         for i in range(54):
             angle = self.theta - scan_angle
-            scan_point = (10*math.cos(angle),  10*math.sin(angle))
+            scan_point = (10*math.cos(math.radians(angle)),  10*math.sin(math.radians(angle)))
             line = LineString([(self.x, self.y), scan_point])
             closest_collision_distance = 10000
-
+    
             #Find closest obstacle for collision:
             for o in obstacles:
                 collision = line.intersection(o)
+                distance = self.compute_distance(collision, o)
+                if distance < closest_collision_distance:
+                    closest_collision_distance = distance
+
+            if closest_collision_distance == 10000:
+                closest_collision_distance = float('NaN')
+
+            hq.heappush(self.distances, closest_collision_distance)
+            scan_angle -= 1.125
+
+    def compute_distance(self, collision, obstacle):
+        linestring_type = LineString([(0,0), (2,2)]).geom_type
+        point_type = Point(0,0).geom_type
+
+        #Intersection with wall
+        if collision.geom_type == point_type:
+            (collision_x, collision_y) = (collision.coords[0][0], collision.coords[0][1])
+            dist = math.sqrt((self.x - collision_x)**2 + (self.y - collision_y)**2) 
+            if dist >= 0.45 and dist <= 10.0:
+                return dist
+            else:
+                return 10000
+
+        #Intersection through obstacle
+        elif collision.geom_type == linestring_type:
+            endpoints = [collision.coords[0], collision.coords[1]]
+            min_dist = 10000
+            for tup in endpoints:
+                (collision_x, collision_y) = (tup[0], tup[1])
+                dist = math.sqrt((self.x - collision_x)**2 + (self.y - collision_y)**2) 
+                min_dist = min(min_dist, dist)
+            if min_dist >= 0.45 and min_dist <= 10.0:
+                return dist
+            else:
+                return 10000
+
+        #MultiLineString Intersection: Intersected disjoint objects
+        else:
+            min_dist = 10000
+            for linestring in collision.geoms:
+                endpoints = (linestring.coords[0], linestring.coords[1])
+                for (collision_x, collision_y) in endpoints:
+                    dist = math.sqrt((self.x - collision_x)**2 + (self.y - collision_y)**2) 
+                    min_dist = min(min_dist, dist)
+            if min_dist >= 0.45 and min_dist <= 10.0:
+                return dist
+            else:
+                return 10000
+            
+        '''            
+        #print 'POINTS:' , points
+        min_dist = 100000
+        for i in range(len(points)):
+            for j in range(len(points[i])):
+                #print points[i][j]
+                dist = math.sqrt((self.x - points[i][j][0])**2 + (self.y - points[i][j][1])**2) 
+                if dist < min_dist:
+                    #print 'Swapping distances. \tCurrent min: ', min_dist, '\tNew min: ', dist
+                    min_dist = dist
+        return dist if (dist >= 0.45 and dist <= 10.0) else float('NaN')
+
                 if collision:
-                    #Compute distance
-                    if type(collision) != type(line):  #MultiLineStringObject (we may have passed through two obstacles)
+                    #MultiLineStringObject (we may have passed through two obstacles) or Point (intersects wall)
+                    if type(collision) != type(line):
+                        #if type(collision) != type(Point(0,0)):
+                        #print type(collision), collision
                         line_endpoints = []
                         for linestring in collision.geoms:
                             endpoints = (linestring.coords[0], linestring.coords[1])
                             line_endpoints.append(endpoints)
+                        #else:
+                        #    line_endpoints = list(collision.coords)
                     else:
                         line_endpoints = [(collision.coords[0], collision.coords[1])]
                     
@@ -108,17 +170,7 @@ class Particle:
             hq.heappush(self.distances, nearest_object_collision)
             scan_angle -= 1.125
 
-    def compute_distance(self, points):
-        #print 'POINTS:' , points
-        min_dist = 100000
-        for i in range(len(points)):
-            for j in range(len(points[i])):
-                #print points[i][j]
-                dist = math.sqrt((self.x - points[i][j][0])**2 + (self.y - points[i][j][1])**2) 
-                if dist < min_dist:
-                    #print 'Swapping distances. \tCurrent min: ', min_dist, '\tNew min: ', dist
-                    min_dist = dist
-        return dist if (dist >= 0.45 and dist <= 10.0) else float('NaN')
+    '''
 
 
 class Message():
